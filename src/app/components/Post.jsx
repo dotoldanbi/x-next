ae5209a8fce8ffac8d2da11631dbff3b3d4e8ca1
jhsong <jh@song>
2025-06-30 17:41:17 +0900

    icons

--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { HiDotsHorizontal } from "react-icons/hi";
 import Link from "next/link";
+import Icons from "./Icons";
 export default function Post({ post, id }) {
   return (
-    <div className="flex p-3 border-b border-gray-200">
+    <div className="flex p-3 border-b border-gray-200 hover:bg-gray-100">
       <img
         src={post?.profileImg}
         alt="user-img"
@@ -28,6 +29,7 @@ export default function Post({ post, id }) {
             className="rounded-2xl mr-2 cursor-pointer"
           />
         </Link>
+        <Icons />
       </div>
     </div>
   );
